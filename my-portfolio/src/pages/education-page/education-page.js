755e9fc0d4c55c2cb04404480a86ee7c35609037
drakenevadie19 <drakennievadje19@gmail.com>
2023-11-28 10:25:13 -0600

    New Change

--- my-portfolio/src/pages/education-page/education-page.js
+++ my-portfolio/src/pages/education-page/education-page.js
@@ -1,28 +1,29 @@
 import SchoolImage from "./schools-image";
 
-const changeImage = ({imagesrc}) => {
-    let image = document.getElementById("imageHere");
-    console.log(image);
-    if (imagesrc != null) {
-        image.src = imagesrc;
-    }
-}
-
 const EducationPage = ({ educationList }) => (
     <>
-        <SchoolImage id="imageHere" srcName={educationList[0].imagesrc} />
-        <div className="education-box">
-            {educationList.map(schools => (
-                <details key={schools.name}>
-                    <summary><h1 onClick={changeImage(schools.imagesrc)}>{schools.name}</h1></summary>
-                    <p>{schools.major}</p>
-                    <p>{schools.location}</p>
-                    <p>{schools.minor}</p>
-                    <p>{schools.gpa}</p>
-                </details>
-            ))}
+        <div className="education"><SchoolImage id="imageHere" srcName={educationList[0].imagesrc} />
+            <div className="education-box">
+                {educationList.map(schools => (
+                    <details key={schools.name}>
+                        <summary><a onClick={changeImage(schools.imagesrc)}><h1>{schools.name}</h1></a></summary>
+                        <p>{schools.major}</p>
+                        <p>{schools.location}</p>
+                        <p>{schools.minor}</p>
+                        <p>{schools.gpa}</p>
+                    </details>
+                ))}
+            </div>
         </div>
     </>
 );
+
+const changeImage = ({imagesrc}) => {
+    let image = document.getElementById("imageHere");
+    console.log(image);
+    // if (imagesrc != null) {
+    //     image = imagesrc;
+    // }
+}
 
 export default EducationPage;

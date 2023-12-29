5fcb488efc8378ffe319bebfbea506718cc45443
Nguyen Do <nguyendo@Nguyens-Air.lan>
2023-12-29 02:12:08 -0600

    New Change: 99% on Contact page

--- my-portfolio/src/pages/contact-page/contact-me.js
+++ my-portfolio/src/pages/contact-page/contact-me.js
@@ -40,25 +40,34 @@ const ContactMe = () => {
 
     return (
         <> 
-          <form onSubmit={handleSubmit}>
-            <div class="row p-4 name-email-box">
-              <div class="w-50 name">
-                  <label for="firstName" class="form-label contact-label">Full Name</label>
-                  <input type="text" class="form-control contact-area" id="firstName" placeholder="Thanh Nguyen" value={formData.firstName} onChange={handleChange} />
-              </div>
-              <div class="w-50 email">
-                  <label for="email-value" class="form-label contact-label">Email</label>
-                  <input type="email" class="form-control contact-area" id="email-value" placeholder="name@example.com"  value={formData.email} onChange={handleChange} />
-              </div>
+          <div className="contact-page">
+            <div className="placeholder-Image">
+              <img src="/programmer.png" alt="Programmer"/>
             </div>
-              
-            <div class="w-100 p-4 box">
-                <label for="message" class="form-label contact-label">Your Message to Thanh Nguyen!!</label>
-                <textarea class="form-control contact-area" id="message" rows="3" placeholder="Hello Thanh Nguyen" value={formData.message} onChange={handleChange}></textarea>
-            </div>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div class="row p-4 name-email-box">
+                  <div class="w-50 name">
+                      <label for="firstName" class="form-label contact-label">Full Name</label>
+                      <input type="text" class="form-control contact-area" id="firstName" placeholder="Thanh Nguyen" value={formData.firstName} onChange={handleChange} />
+                  </div>
+                  <div class="w-50 email">
+                      <label for="email-value" class="form-label contact-label">Email</label>
+                      <input type="email" class="form-control contact-area" id="email-value" placeholder="name@example.com"  value={formData.email} onChange={handleChange} />
+                  </div>
+                </div>
+                  
+                <div class="w-100 p-4 box">
+                    <label for="message" class="form-label contact-label">Your Message to Thanh Nguyen!!</label>
+                    <textarea class="form-control contact-area" id="message" rows="3" placeholder="Hello Thanh Nguyen" value={formData.message} onChange={handleChange}></textarea>
+                </div>
 
-            <button type="submit" class="btn btn-primary"> Send</button>
-          </form>
+                <button type="submit" class="btn btn-primary"> Send</button>
+              </form>
+            </div>
+          </div>
+          
+          
       </>
     )
 }

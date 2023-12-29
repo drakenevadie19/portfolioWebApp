99152c4f69a4ab29638851498e6e4c439f1f03f6
Nguyen Do <nguyendo@Nguyens-Air.lan>
2023-12-29 01:16:23 -0600

    New Change: all page are good with responsive, but contact page need 10%more on input space

--- my-portfolio/src/pages/contact-page/contact-me.js
+++ my-portfolio/src/pages/contact-page/contact-me.js
@@ -1,65 +1,65 @@
 import React, { useState } from "react";
 
 const ContactMe = () => {
-    const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        message: ''
-      });    
+  const [formData, setFormData] = useState({
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: ''
+    });    
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-      };
-    
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-        try {
-          const response = await fetch('http://localhost:3001/send-email', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-          });
-    
-          if (response.ok) {
-            console.log('Email sent successfully');
-            // Optionally, reset the form or show a success message to the user
-          } else {
-            console.error('Failed to send email');
-            // Handle error, show error message to the user, etc.
-          }
-        } catch (error) {
-          console.error('Error:', error);
-          // Handle network error, show error message to the user, etc.
+  const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      try {
+        const response = await fetch('http://localhost:3001/send-email', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+  
+        if (response.ok) {
+          console.log('Email sent successfully');
+          // Optionally, reset the form or show a success message to the user
+        } else {
+          console.error('Failed to send email');
+          // Handle error, show error message to the user, etc.
         }
-      };
+      } catch (error) {
+        console.error('Error:', error);
+        // Handle network error, show error message to the user, etc.
+      }
+    };
+
     return (
         <> 
-            <form onSubmit={handleSubmit}>
-                <div class="mb-3">
-                    <label for="firstName" class="form-label contact-label">First Name</label>
-                    <input type="text" class="form-control contact-area" id="firstName" placeholder="Thanh Nguyen" value={formData.firstName} onChange={handleChange} />
-                </div>
-                <div class="mb-3">
-                    <label for="lastName" class="form-label contact-label">Last Name</label>
-                    <input type="text" class="form-control contact-area" id="lastName" placeholder="Do" value={formData.lastName} onChange={handleChange} />
-                </div>
-                <div class="mb-3">
-                    <label for="email-value" class="form-label contact-label">Email address</label>
-                    <input type="email" class="form-control contact-area" id="email-value" placeholder="name@example.com"  value={formData.email} onChange={handleChange} />
-                </div>
-                <div class="mb-3">
-                    <label for="message" class="form-label contact-label">Your Message to Thanh Nguyen!!</label>
-                    <textarea class="form-control contact-area" id="message" rows="3" placeholder="Hello Thanh Nguyen" value={formData.message} onChange={handleChange}></textarea>
-                </div>
+          <form onSubmit={handleSubmit}>
+            <div class="row p-4 name-email-box">
+              <div class="w-50 name">
+                  <label for="firstName" class="form-label contact-label">Full Name</label>
+                  <input type="text" class="form-control contact-area" id="firstName" placeholder="Thanh Nguyen" value={formData.firstName} onChange={handleChange} />
+              </div>
+              <div class="w-50 email">
+                  <label for="email-value" class="form-label contact-label">Email</label>
+                  <input type="email" class="form-control contact-area" id="email-value" placeholder="name@example.com"  value={formData.email} onChange={handleChange} />
+              </div>
+            </div>
+              
+            <div class="w-100 p-4 box">
+                <label for="message" class="form-label contact-label">Your Message to Thanh Nguyen!!</label>
+                <textarea class="form-control contact-area" id="message" rows="3" placeholder="Hello Thanh Nguyen" value={formData.message} onChange={handleChange}></textarea>
+            </div>
 
-                <button type="submit" class="btn btn-primary"> Send</button>
-            </form>
-        </>
+            <button type="submit" class="btn btn-primary"> Send</button>
+          </form>
+      </>
     )
 }
 
--- my-portfolio/src/pages/home-page/home-page.js
+++ my-portfolio/src/pages/home-page/home-page.js
@@ -17,7 +17,7 @@ const HomePage = () => (
                 <div className="animated_text">
                     <h1 className="title">Front - end / Full - stack Developer</h1>
                 </div>
-                <span>My name is Thanh Nguyen (William) Do. I am from Hai Phong, Vietnam, currently in Fort Worth, Texas. 
+                <span>My name is Thanh Nguyen (Williams) Do. I am from Hai Phong, Vietnam, currently in Fort Worth, Texas. 
                     I'm a beginner full stack web developer, currently I have some experiences with <b>HTML&Bootstrap, CSS&LESS, JavaScript&TypeScript, and NodeJS</b>. 
                     Now I practice in the development of web applications, and some frameworks like Angular, React, Vue etc. 
                     I want to learn more and utilize my skills in the field of Web Developing and to help solve real world challenges. </span>

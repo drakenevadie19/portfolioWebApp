316cd2c2a2a48b66b6356bdb2457e9142d23ccd0
Nguyen Do <nguyendo@Nguyens-Air.lan>
2023-12-30 17:27:21 -0600

    New Change: 99.5% FE

--- my-portfolio/src/pages/Routing-Pages/start-page.js
+++ my-portfolio/src/pages/Routing-Pages/start-page.js
@@ -50,7 +50,7 @@ function StartPage() {
           </span>
         </h6>
         
-        <button type="button" class="btn btn-primary" onClick={handleClick}>
+        <button type="button" class="btn btn-primary view-more-btn" onClick={handleClick}>
           View more about me
           <span>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb" viewBox="0 0 16 16">
@@ -64,7 +64,7 @@ function StartPage() {
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-c-circle" viewBox="0 0 16 16">
           <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512Z"/>
         </svg>
-        <h5>2023 Thanh Nguyen Do</h5>
+        <h6>2023 Thanh Nguyen Do</h6>
       </div>
     </div>
   );
--- my-portfolio/src/pages/contact-page/contact-me.js
+++ my-portfolio/src/pages/contact-page/contact-me.js
@@ -1,79 +1,105 @@
 import React, { useState } from "react";
-
 import CopyRight from "../Routing-Pages/copyright-component";
 
 const ContactMe = () => {
   const [formData, setFormData] = useState({
-      firstName: '',
-      lastName: '',
-      email: '',
-      message: ''
-    });    
+    name: '',
+    email: '',
+    message: ''
+  });
+  const [submitError, setSubmitError] = useState(null);
 
   const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData({ ...formData, [name]: value });
-    };
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      try {
-        const response = await fetch('http://localhost:3001/send-email', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        });
-  
-        if (response.ok) {
-          console.log('Email sent successfully');
-          // Optionally, reset the form or show a success message to the user
-        } else {
-          console.error('Failed to send email');
-          // Handle error, show error message to the user, etc.
-        }
-      } catch (error) {
-        console.error('Error:', error);
-        // Handle network error, show error message to the user, etc.
+  const handleSubmit = async (e) => {
+    console.log(formData);
+    console.log('Form submitted!');
+    e.preventDefault();
+
+    try {
+      console.log('Sending form data:', formData); // Log form data for debugging
+
+      const response = await fetch('http://localhost:3001/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log('Email sent successfully');
+        setSubmitError(null);
+        // Optionally, reset the form or show a success message to the user
+      } else {
+        const errorData = await response.json(); // Try to parse error details
+        console.error('Failed to send email:', response.status, errorData);
+        setSubmitError(errorData.message || 'Failed to send email. Please try again.');
       }
-    };
+    } catch (error) {
+      console.error('Error:', error);
+      setSubmitError('Network error. Please try again.'); // Set an error message for the user
+    }
+  };
 
-    return (
-      <> 
-        <div id="page-body">
-          <div className="contact-page">
-            <div className="placeholder-Image">
-              <img src="/programmer.png" alt="Programmer"/>
-            </div>
-            <div>
-              <form onSubmit={handleSubmit}>
-                <div class="row p-4 name-email-box">
-                  <div class="w-50 name">
-                      <label for="firstName" class="form-label contact-label">Full Name</label>
-                      <input type="text" class="form-control contact-area" id="firstName" placeholder="Thanh Nguyen" value={formData.firstName} onChange={handleChange} />
-                  </div>
-                  <div class="w-50 email">
-                      <label for="email-value" class="form-label contact-label">Email</label>
-                      <input type="email" class="form-control contact-area" id="email-value" placeholder="name@example.com"  value={formData.email} onChange={handleChange} />
-                  </div>
-                </div>
-                  
-                <div class="w-100 p-4 box">
-                    <label for="message" class="form-label contact-label">Your Message to Thanh Nguyen!!</label>
-                    <textarea class="form-control contact-area" id="message" rows="3" placeholder="Hello Thanh Nguyen" value={formData.message} onChange={handleChange}></textarea>
-                </div>
-
-                <button type="submit" class="btn btn-primary"> Send</button>
-              </form>
-            </div>
+  return (
+    <>
+      <div id="page-body">
+        <div className="contact-page">
+          <div className="placeholder-Image">
+            <img src="/programmer.png" alt="Programmer"/>
           </div>
+          <div className="contact-info">
+            <p>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-telephone-forward" viewBox="0 0 16 16">
+                  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zm10.762.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708z"/>
+                </svg>
+              </span>
+              <b>(682)-374-8227</b>
+            </p>
 
-          <CopyRight />
+            <p>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                </svg>
+              </span>
+              <b>do.t.nguyen@tcu.edu</b>
+            </p>
+          </div>
+          <div className="form-manage">
+            <form onSubmit={handleSubmit}>
+              <div className="row p-4 name-email-box">
+                {/* value={formData.name}  */}
+                <div className="w-50 name">
+                  <label htmlFor="name" className="form-label contact-label">Full Name</label>
+                  <input type="text" name="name" className="form-control contact-area" id="name" value={formData.name} onChange={handleChange} />
+                </div>
+                <div className="w-50 email">
+                  <label htmlFor="email" className="form-label contact-label">Email</label>
+                  <input type="email" name="email" className="form-control contact-area" id="email" value={formData.email} onChange={handleChange} />
+                </div>
+              </div>
+
+              <div className="w-100 p-4 box">
+                <label htmlFor="message" className="form-label contact-label">Your Message to Thanh Nguyen!!</label>
+                <textarea name="message" className="form-control contact-area" id="message" rows="3" value={formData.message} onChange={handleChange}></textarea>
+              </div>
+
+              <button type="submit" className="btn btn-primary">Send</button>
+            </form>
+            {submitError && <div className="error-message">{submitError}</div>}
+          </div>
         </div>
-      </>
-    )
-}
+
+        <CopyRight />
+      </div>
+    </>
+  );
+};
 
 export default ContactMe;

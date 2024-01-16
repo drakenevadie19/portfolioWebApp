7a9235f337357e8608c7a49413cff28024dbcc85
drakenevadie19 <drakennievadje19@gmail.com>
2024-01-16 11:18:56 -0600

    New Change: Adding blogapp, twotree and contactInterface for project page

--- my-portfolio/src/pages/project-page/project-data.js
+++ my-portfolio/src/pages/project-page/project-data.js
@@ -1,19 +1,38 @@
 const projectData = [
     {
+        name: "Blog-app", 
+        link: "https://github.com/drakenevadie19/BlogApp.git",
+        src: "/project-bio-image/BlogApp.jpg",
+        description: "Architectured a website blog application allowing users to save memories and articles.",
+        participants: "Drake Do",
+    },
+    {
+        name: "Two Tree Ecommerce App",
+        link: "https://github.com/drakenevadie19/TwoTreeEcommerceApp.git",
+        src: "/project-bio-image/ecommerceApp.png",
+        description: "Designed an fullstack interface with MEVN-stack for shopping demands of users with fully necessary functions for a shopping page. I am still working on this page to develop more.",
+        participants: "Drake Do",
+    },
+    {
+        name: "Contact Interface",
+        link: "https://github.com/drakenevadie19/ContactInterface.git",
+        src: "/project-bio-image/contactInterface.png",
+        description: "Designed an fullstack interface using React and Spring Boot to store and manage contacts",
+        participants: "Drake Do",
+    },
+    {
         name: "Hogwart-artifact-online",
         link: "https://github.com/drakenevadie19/hogwart-artifact-online",
         src: "/project-bio-image/hogwart.jpg",
         description: "Developed a fully-fledged RESTful API server to manage wizard object entity and artifact object entity using SpringBoot, Spring MVC, Spring Data JPA and H2 database.",
         participants: "Drake Do",
-        lessInfo: "true"
     },
     {
         name: "sudokuGame",
         link: "https://github.com/drakenevadie19/sudokuGame",
         src: "/project-bio-image/sudokuGame.jpg",
-        description: "Inspired by traditional sudoku game, I design a web page for users to solve their maze, and also play Sudoku there. Create a responsive design website, using Angular framework, with Material UI.",
+        description: "Inspired by traditional sudoku game, I design a web page for users to solve their maze, and also play Sudoku there. I created a responsive design website, using Angular framework, with Material UI.",
         participants: "Drake Do",
-        lessInfo: "true"
     },
     {
         name: "SuperFrog Scheduler",
@@ -21,7 +40,6 @@ const projectData = [
         src: "/project-bio-image/superFrog.jpg",
         description: "Increased website traffic 20% by designing a responsive Vue.js-based front-end for TCU Recreational Center's customer-facing scheduling system, allowing bookings of mascot appearances.",
         participants: "Drake Do",
-        lessInfo: "true"
     },
     {
         name: "Book Manager Database",
@@ -29,7 +47,6 @@ const projectData = [
         src: "/project-bio-image/books.jpg",
         description: "Guided Dr. Sarka in TCU, I developed a database system for managing new books coming for TCU, using Python and MySQL. This database can also handle queries such as searching a book/books, changing books' information, deleting books. For IDE, I used Pycharm for Python and Datagrip, with Docker to monitor database.",
         participants: "Drake Do",
-        lessInfo: "true"
     },
 ]
 

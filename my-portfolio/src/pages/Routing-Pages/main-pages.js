55148c2f9d2aea426d2380620eeae9766e5283b8
drakenevadie19 <drakennievadje19@gmail.com>
2024-03-07 14:30:42 -0600

    New Change: Add button to send email by icloud server

--- my-portfolio/src/pages/Routing-Pages/main-pages.js
+++ my-portfolio/src/pages/Routing-Pages/main-pages.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../nav-bar';
-import CopyRight from './copyright-component';
-
 function MainPages() {
   return (
     <>
@@ -11,7 +9,6 @@ function MainPages() {
           <Navbar />
           <Outlet />
         </div>
-        <CopyRight />
       </div>
     </>
     

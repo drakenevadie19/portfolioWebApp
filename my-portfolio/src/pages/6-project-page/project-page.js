539d3f9df6b22708cb0b11341f7b373c22c3b85a
drakenevadie19 <drakennievadje19@gmail.com>
2025-01-24 22:32:49 -0600

    New Change: Added Filter button

--- my-portfolio/src/pages/6-project-page/project-page.js
+++ my-portfolio/src/pages/6-project-page/project-page.js
@@ -171,12 +171,14 @@ const ProjectPage = ({ projects }) => {
           </div> */}
 
           <div className="project-list-tab">
-            {/* <div className="filter-button">
+            <h3 className="project-list-tab-header">
+              My projects ({numberOfToRenderProjects})
+            </h3>
+            <div className="filter-button">
               {usingFilter === false ? (
-                // <a className='tech-stack-element-filter-button'>
                 <button
                   type="button"
-                  className="btn btn-outline-success tech-stack-element-filter-button"
+                  className="btn btn-outline-primary tech-stack-element-filter-button"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -188,13 +190,13 @@ const ProjectPage = ({ projects }) => {
                   >
                     <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z" />
                   </svg>{" "}
-                  Filter
+                  Filter projects
                 </button>
               ) : (
                 // <a className='tech-stack-element' onClick={resetFilter}>
                 <button
                   type="button"
-                  className="btn btn-success tech-stack-element-filter-button"
+                  className="btn btn-outline-danger tech-stack-element-filter-button"
                   onClick={resetFilter}
                 >
                   <svg
@@ -210,11 +212,8 @@ const ProjectPage = ({ projects }) => {
                   Reset filter
                 </button>
               )}
-            </div> */}
-
-            <h3 className="project-list-tab-header">
-              My projects ({numberOfToRenderProjects})
-            </h3>
+            </div>
+            
             <div className="project-list-tab-list-of-projects-onSmallDevice">
               <button
                 type="button"
